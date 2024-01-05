9a67d7f47975bf903e2a1f61c69da17d30d1b58f
Ismoiljon <ibahtiyorov007@gmail.com>
2024-01-06 03:50:00 +0500

    last upaksjdjflkasd

--- app.vue
+++ app.vue
@@ -1,23 +1,13 @@
 <template>
-  <div class="overlay" v-if="store.overlay" @click="store.overlay = false, store.search_open = false"></div>
-  <!-- <NuxtLayout /> -->
-  <loading />
-  <div class="add" v-if="add">
-    <img src="@/assets/images/png/reklama.png" alt="">
-    <button @click="add = false">
-      <img src="@/assets/images/svg/close-ad.svg" alt="">
-    </button>
+  <div>
+
   </div>
 </template>
+
 <script setup>
-import { useStore } from '~/store/store';
-const store = useStore()
-const add = ref(false)
-const router = useRouter()
-store.loader = true
-onMounted(() => {
-  setTimeout(() => {
-    // add.value = true
-  }, 120000)
-})
+
 </script>
+
+<style lang="scss" scoped>
+
+</style>
