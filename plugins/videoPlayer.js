9eb00ac188e4a9e9172b8499a094e25340706702
Ismoiljon <ibahtiyorov007@gmail.com>
2024-01-06 02:47:19 +0500

    update last

--- plugins/videoPlayer.js
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from "#app";
-import VueVideoPlayer from "@videojs-player/vue"
-import "video.js/dist/video-js.css"
-export default defineNuxtPlugin(nuxtApp => {
-    nuxtApp.vueApp.use(VueVideoPlayer)
-})
