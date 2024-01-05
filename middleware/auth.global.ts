613008a05c0a12db69fc54f2cdc85ba9450b99df
Ismoiljon <ibahtiyorov007@gmail.com>
2024-01-06 03:06:12 +0500

    update

--- middleware/auth.global.ts
+++
@@ -1,7 +0,0 @@
-import { useStore } from "~~/store/store";
-export default defineNuxtRouteMiddleware((to, from) => {
-  const store = useStore();
-  if (store.token && to.path == "/login") {
-    return navigateTo("/");
-  }
-});
