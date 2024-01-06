e2861be7f06fd5688fd661f69eaf95b5e648fbeb
Ismoiljon <ibahtiyorov007@gmail.com>
2024-01-06 12:19:56 +0500

    update

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
-  <div  v-if="store.overlay" @click="store.overlay = false, store.search_open = false"></div>
+  <div>
+
+  </div>
 </template>
+
 <script setup>
-import { useStore } from '~/store/store';
-const store = useStore()
-const add = ref(false)
+
 </script>
+
+<style lang="scss" scoped>
+
+</style>
