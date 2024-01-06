734db369314379d372c338fa25e65cc34b2ad33b
Ismoiljon <ibahtiyorov007@gmail.com>
2024-01-06 05:32:00 +0500

    UPDATEASDF

--- app.vue
+++ app.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="overlay" v-if="store.overlay" @click="store.overlay = false, store.search_open = false"></div>
-  <!-- <NuxtLayout /> -->
-  <!-- <loading /> -->
-  <div class="add" v-if="add">
-    <img src="@/assets/images/png/reklama.png" alt="">
-    <button @click="add = false">
-      <img src="@/assets/images/svg/close-ad.svg" alt="">
-    </button>
-  </div>
 </template>
 <script setup>
 import { useStore } from '~/store/store';
 const store = useStore()
 const add = ref(false)
-const router = useRouter()
 </script>
