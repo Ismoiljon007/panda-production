9befdd6c352c8fb837b687ca2fb4402e530c6cfa
Ismoiljon <ibahtiyorov007@gmail.com>
2024-01-06 18:03:11 +0500

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@pinia/nuxt", "nuxt-swiper", "@nuxt/ui"],
-  css: ["~/assets/scss/main.scss"],
+  css: ["@/assets/scss/main.scss"],
   devtools: { enabled: false },
   routeRules: {
     "/categorie/**": { ssr: false },
