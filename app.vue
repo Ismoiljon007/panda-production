86634c6afa18e0888a809bdd79f44cd7ac45c336
Ismoiljon <ibahtiyorov007@gmail.com>
2024-01-07 16:24:07 +0500

    update

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <Head>
-        <Title>Panda Production</Title>
         <Link rel="icon" href="/panda-icon-2.png" type="image/x-icon" sizes="16x16" />
     </Head>
   <div class="overlay" v-if="store.overlay" @click="store.overlay = false, store.search_open = false"></div>
@@ -15,6 +14,18 @@
 </template>
 <script setup>
 import { useStore } from '~/store/store';
+useHead({
+  title: 'PandaTV.uz - kino va seriallar eng yaxshi sifatda.',
+  meta: [
+    { hid: 'description', name: 'description', content: "PandaTV.uz - har qanday didga mos keluvchi keng kontent tanloviga ega bo'lgan ko'ngilochar platforma." },
+    { hid: 'og:title', property: 'og:title', content: 'PandaTV.uz - kino va seriallar eng yaxshi sifatda.' },
+    { hid: 'og:description', property: 'og:description', content: "PandaTV.uz - har qanday didga mos keluvchi keng kontent tanloviga ega bo'lgan ko'ngilochar platforma." },
+    { hid: 'og:image', property: 'og:image', content:  '/panda-icon-2.png'},
+    { hid: 'og:url', property: 'og:url', content: 'pandatv.uz' },
+    { hid: 'telegram:channel', property: 'telegram:channel', content: 'https://t.me/azamataxrorov1' },
+    // Add other meta tags as needed
+  ],
+})
 const store = useStore()
 const add = ref(false)
 const router = useRouter()
