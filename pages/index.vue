ee912407d9bea83152c3f9897dd0468e1dcf4407
Ismoiljon <ibahtiyorov007@gmail.com>
2024-01-07 22:23:14 +0500

    last changes

--- pages/index.vue
+++ pages/index.vue
@@ -96,17 +96,17 @@ import videojs from 'video.js';
 import { useStore } from '~~/store/store';
 const store = useStore()
 store.loader = true
-// useHead({
-//     title: "PandaTV.uz - kino va seriallar eng yaxshi sifatda.",
-//     meta: [
-//         { hid: 'description', name: 'description', content: "PandaTV.uz - har qanday didga mos keluvchi keng kontent tanloviga ega bo'lgan ko'ngilochar platforma." },
-//         { hid: 'og:title', "PandaTV.uz - kino va seriallar eng yaxshi sifatda.": 'og:title', content: "PandaTV.uz - kino va seriallar eng yaxshi sifatda." },
-//         { hid: 'og:description', content: "PandaTV.uz - har qanday didga mos keluvchi keng kontent tanloviga ega bo'lgan ko'ngilochar platforma." 'og:description', content: "PandaTV.uz - har qanday didga mos keluvchi keng kontent tanloviga ega bo'lgan ko'ngilochar platforma." },
-//         { hid: 'og:image', '/panda-icon-2.png': 'og:image', content: '/panda-icon-2.png' },
-//         { hid: 'og:url', 'https://pandatv.uz': 'og:url', content: 'https://pandatv.uz' },
-//         { hid: 'telegram:channel', 'https://t.me/azamataxrorov1': 'telegram:channel', content: 'https://t.me/azamataxrorov1' },
-//     ],
-// })
+useHead({
+    title: "PandaTV.uz - kino va seriallar eng yaxshi sifatda.",
+    meta: [
+        { hid: 'description', name: 'description', content: "PandaTV.uz - har qanday didga mos keluvchi keng kontent tanloviga ega bo'lgan ko'ngilochar platforma." },
+        { hid: 'og:title', property: 'og:title', content: "PandaTV.uz - kino va seriallar eng yaxshi sifatda." },
+        { hid: 'og:description', property: 'og:description', content: "PandaTV.uz - har qanday didga mos keluvchi keng kontent tanloviga ega bo'lgan ko'ngilochar platforma." },
+        { hid: 'og:image', property: 'og:image', content: '/panda-icon-2.png' },
+        { hid: 'og:url', property: 'og:url', content: 'https://pandatv.uz' },
+        { hid: 'telegram:channel', property: 'telegram:channel', content: 'https://t.me/azamataxrorov1' },
+    ],
+})
 const banners = ref(null)
 async function getBanners() {
     store.loader = true
